111dcd49e3f8253380dcf69df3ea6c1b79d73c10
StuGeek <978475534@qq.com>
2021-12-02 16:56:34 +0000

    删除文件 Homework4-HitUFO

--- Homework4-HitUFO/Assets/Scripts/DiskData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DiskData : MonoBehaviour {
-    public int size; // 大小
-    public string color; // 颜色
-    public int speed; // 发射速度
-}
